c003e4bb2c74fde26f758d0a0da81e967a4d5ea3
gitesh.mane21@vit.edu <giteshmane013>
2023-08-24 14:21:27 +0530

    addded

--- OS_CP_JS.js
+++ OS_CP_JS.js
@@ -113,3 +113,71 @@ function sjfSchedulerVisualize(processes) {
         completedProcesses++;
     }
 }
+// priorty --incomplete
+function prioritySchedulerVisualize(processes) {
+    const timelineElement = document.getElementById('timeline');
+    timelineElement.innerHTML = ''; 
+    const animationDuration = 500; 
+  
+    let currentTime = 0;
+    const numProcesses = processes.length;
+  
+    while (true) {
+        let highestPriorityIndex = -1;
+  
+        for (let i = 0; i < numProcesses; i++) {
+            if (processes[i].arrival_time <= currentTime && !processes[i].completed) {
+                if (highestPriorityIndex === -1 || processes[i].priority < processes[highestPriorityIndex].priority) {
+                    highestPriorityIndex = i;
+                }
+            }
+        }
+  
+        if (highestPriorityIndex === -1) {
+            let allCompleted = true;
+            for (let i = 0; i < numProcesses; i++) {
+                if (!processes[i].completed) {
+                    allCompleted = false;
+                    break;
+                }
+            }
+            if (allCompleted) {
+                break;
+            }
+  
+            const nextArrival = Math.min(...processes.filter(p => !p.completed).map(p => p.arrival_time));
+            const idleElement = document.createElement('div');
+            idleElement.textContent = `Idle ${currentTime} - ${nextArrival}`;
+            idleElement.className = 'event idle';
+            timelineElement.appendChild(idleElement);
+            currentTime = nextArrival;
+        } else {
+            const process = processes[highestPriorityIndex];
+  
+            if (currentTime < process.arrival_time) {
+                const idleElement = document.createElement('div');
+                idleElement.textContent = `Idle ${currentTime} - ${process.arrival_time}`;
+                idleElement.className = 'event idle';
+                timelineElement.appendChild(idleElement);
+                currentTime = process.arrival_time;
+            }
+  
+            const processElement = document.createElement('div');
+            processElement.textContent = `P${process.pid} (${currentTime} - ${currentTime + process.burst_time})`;
+            processElement.className = 'event';
+            timelineElement.appendChild(processElement);
+  
+            setTimeout(() => {
+                processElement.classList.add('active');
+            }, currentTime * animationDuration);
+  
+            setTimeout(() => {
+                processElement.classList.remove('active');
+                process.completed = true;
+            }, (currentTime + process.burst_time) * animationDuration);
+  
+            currentTime += process.burst_time;
+        }
+    }
+  }
+  
